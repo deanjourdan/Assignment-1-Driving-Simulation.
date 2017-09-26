8296d0e429cd1e43843befb4797ca25c39a948e6
deanjourdan <deanjourdandumais@gmail.com>
2017-09-26 08:38:44 +0700

    first commit

---
+++ Driving Simulation.py
@@ -0,0 +1,22 @@
+Vo = 0
+t = int(input("Time: "))
+distance = int(input("Distance: "))
+speed_limit = 60
+a = int(input("acceleration: "))
+v = Vo + a*t
+s = Vo*t + 1/2*a*t**2
+
+for i in range(0,t+1):
+    s = Vo*i + 1/2*a*i**2
+    print("Duration" + str(i) + ':' + "Distance" + " "+'*'*i)
+
+if v > speed_limit:
+    print('The person went over the speed limit,'+ "" +'Max speed' + str(speed_limit)+ " " + 'm/s')
+else:
+    print('The person did not go over the speed limit,' + 'Max speed' + str(speed_limit)+ " " + 'm/s')
+
+if s >= distance:
+    print('The person reached the destination,'+ " " +'Reached'+ " " +str(s), "m")
+
+else:
+    print('The person did not reached the destination,'+ " " +'Reached'+ " " +str(s), "m")
