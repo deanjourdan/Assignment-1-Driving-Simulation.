70840e937834662c2657c2fe036fd28ee8010ec0
deanjourdan <deanjourdandumais@gmail.com>
2017-10-03 08:32:24 +0700

    Assignments2

--- Driving Simulation.py
+++ Driving Simulation.py
@@ -6,14 +6,16 @@ a = int(input("acceleration: "))
 v = Vo + a*t
 s = Vo*t + 1/2*a*t**2
 
-for i in range(0,t+1):
+for i in range(t):
+    i = i + 1
     s = Vo*i + 1/2*a*i**2
-    print("Duration" + str(i) + ':' + "Distance" + " "+'*'*i)
+    dis = int(s/10) * '*'
+    print("Duration" + str(i) + ':' + "Distance" + " "+str(dis))
 
 if v > speed_limit:
     print('The person went over the speed limit,'+ "" +'Max speed' + str(speed_limit)+ " " + 'm/s')
 else:
-    print('The person did not go over the speed limit,' + 'Max speed' + str(speed_limit)+ " " + 'm/s')
+    print('The person did not go over the speed limit,' + 'Max speed' + str(v)+ " " + 'm/s')
 
 if s >= distance:
     print('The person reached the destination,'+ " " +'Reached'+ " " +str(s), "m")
